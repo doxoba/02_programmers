719fab8b6133494308e25a0070916024d46e86d4
HYEL <hyeri9216@gmail.com>
2026-09-22 13:39:36 +0900

    문제풀이

--- 051_문자개수세기.py
+++ 051_문자개수세기.py
@@ -3,7 +3,12 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/181902
 # ---------------------------------------------------------
 # 문제:
-# 알파벳 대소문자로만 이루어진 문자열 `my_string`이 주어질 때, `my_string`에서 'A'의 개수, `my_string`에서 'B'의 개수,..., `my_string`에서 'Z'의 개수, `my_string`에서 'a'의 개수, `my_string`에서 'b'의 개수,..., `my_string`에서 'z'의 개수를 순서대로 담은 길이 52의 정수 배열을 return 하는 solution 함수를 작성해 주세요.
+# 알파벳 대소문자로만 이루어진 문자열 `my_string`이 주어질 때, 
+# `my_string`에서 'A'의 개수, `my_string`에서 'B'의 개수,..., 
+# `my_string`에서 'Z'의 개수, `my_string`에서 'a'의 개수, 
+# `my_string`에서 'b'의 개수,..., 
+# `my_string`에서 'z'의 개수를 순서대로 담은 길이 52의 정수 배열을 
+# return 하는 solution 함수를 작성해 주세요.
 # ----------------------------------------
 # [제한사항]
 # - 1 ≤ `my_string`의 길이 ≤ 1,000
